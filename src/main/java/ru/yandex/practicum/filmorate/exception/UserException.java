ebf37f5bdb7e48ec0ec18d8a5ea787c44a56da1a
Valleyker <76499013+Valleyker@users.noreply.github.com>
2022-07-24 06:53:31 +0300

    Delete src directory

--- src/main/java/ru/yandex/practicum/filmorate/exception/UserException.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-        import java.io.IOException;
-
-public class UserException extends IOException {
-    public UserException(String message) {
-        super(message);
-    }
-}
